1b3d91986b8cfed08fbe510edaf352eaca0aeeac
Nikolay Avramenko <avr.nic@ya.ru>
2022-11-17 22:11:32 +0300

    homework 1

--- lection_2/hw/converter/converter/1.go
+++ lection_2/hw/converter/converter/1.go
@@ -1,2 +1,31 @@
 package converter
 
+type Converter interface {
+	IntToRoman(number int) string
+}
+
+type converter struct {
+	digits  []int
+	symbols []string
+}
+
+// IntToRoman метод преобразования числа из десятичной системы в римскую
+func (c *converter) IntToRoman(number int) string {
+	var roman string
+	// цикл по словарю
+	for idx, digit := range c.digits {
+		// пока число больше или равно значению из словаря
+		for number >= digit {
+			// вычитаем значение из числа
+			number -= digit
+			// добавляем символ
+			roman += c.symbols[idx]
+		}
+	}
+	return roman
+
+}
+
+func NewConverter(values []int, symbols []string) Converter {
+	return &converter{values, symbols}
+}
